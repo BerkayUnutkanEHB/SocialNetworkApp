6f73cf6211528eb65e9826a46b9da75e8ee8e882
BerkayUnutkanEHB <berkay.unutkan@student.ehb.be>
2024-10-23 13:52:33 +0200

    test commit

---
+++ SocialNetworkApp/SocialNetworkApp/SocialNetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkAppApp.swift
+//  SocialNetworkApp
+//
+//  Created by Berkay Unutkan on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
